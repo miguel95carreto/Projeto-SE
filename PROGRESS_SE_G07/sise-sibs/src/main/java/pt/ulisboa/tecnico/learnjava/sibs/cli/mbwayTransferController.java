739efd4d6b030993eff5405f4c96b70681b6f96b
miguel95carreto <60737847+miguel95carreto@users.noreply.github.com>
2020-02-06 12:20:46 +0000

    Versao2

--- PROGRESS_SE_G07/sise-sibs/src/main/java/pt/ulisboa/tecnico/learnjava/sibs/cli/mbwayTransferController.java
+++ PROGRESS_SE_G07/sise-sibs/src/main/java/pt/ulisboa/tecnico/learnjava/sibs/cli/mbwayTransferController.java
@@ -7,21 +7,20 @@ import pt.ulisboa.tecnico.learnjava.sibs.exceptions.OperationException;
 import pt.ulisboa.tecnico.learnjava.sibs.exceptions.SibsException;
 
 public class mbwayTransferController {
-	Services services;
-	Sibs sibs;
+//Guideline 1: 
+	Services services = new Services();
+	Sibs sibs = new Sibs(100, this.services);
 
 	public void mbway_transfer(String sourcePhoneNumber, String targetPhoneNumber, String amount)
 			throws SibsException, AccountException, OperationException {
-		services = new Services();
-		sibs = new Sibs(100, services);
+
 		if (MbWay.mbWayClients.containsKey(sourcePhoneNumber) && MbWay.mbWayClients.containsKey(targetPhoneNumber)) {
-			int value = Integer.parseInt(amount);
 			String sourceIban = MbWay.mbWayClients.get(sourcePhoneNumber);
 			String targetIban = MbWay.mbWayClients.get(targetPhoneNumber);
-			if (services.getAccountByIban(sourceIban).getClient().getMbwayState().equals("Active")
-					&& services.getAccountByIban(targetIban).getClient().getMbwayState().equals("Active")) {
-				sibs.transfer(sourceIban, targetIban, value);
-				sibs.processOperations();
+			if (this.services.getAccountByIban(sourceIban).getClient().getMbwayState().equals("Active")
+					&& this.services.getAccountByIban(targetIban).getClient().getMbwayState().equals("Active")) {
+				this.sibs.transfer(sourceIban, targetIban, Integer.parseInt(amount));
+				this.sibs.processOperations();
 				System.out.println("Transfer successful!");
 			} else {
 				System.out.println("Could not complete transfer!");
